7f09ba1e2e5af2b9da243c43f855d4d3d8c36ff9
Mandeep Singh <81529956+MandeepSingh396@users.noreply.github.com>
2022-08-06 15:15:04 -0400

    Time: 0 ms (100.00%), Space: 6.4 MB (82.04%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    map<char, string> m;
+    public:
+    void backtrack(string current, int index, string digits, vector<string>& res){
+        if(current.size() >= digits.size())
+        {
+            res.push_back(current);
+            return;
+        }
+        for(char &sym : m[digits[index]])
+            backtrack(current + sym, index+1, digits, res);
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+        
+        m['2'] = "abc";
+        m['3'] = "def";
+        m['4'] = "ghi";
+        m['5'] = "jkl";
+        m['6'] = "mno";
+        m['7'] = "pqrs";
+        m['8'] = "tuv";
+        m['9'] = "wxyz";
+        
+        vector<string> res;
+        if(digits.size() == 0)
+            return res;
+        backtrack("", 0, digits, res);
+        return res;
+    }
+};
